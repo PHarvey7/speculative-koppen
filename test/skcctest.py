6850f9d09ee31b52d9f2afbc684a0389b8161792
Patrick Harvey <sevenconstellation@gmail.com>
2019-05-04 16:34:28 -0700

    Added Boxes test cases to skcctest

--- test/skcctest.py
+++ test/skcctest.py
@@ -20,6 +20,12 @@ def getParentDirPath(fname):
 def getProfiaInputs():
     return getTestDirPath('ProfiaTempJul.png'), getTestDirPath('ProfiaTempJan.png'), getTestDirPath('ProfiaPrecJul.png'), getTestDirPath('ProfiaPrecJan.png')
 
+def getBoxesInputs():
+    return getTestDirPath('BoxesTempJul.png'), getTestDirPath('BoxesTempJan.png'), getTestDirPath('BoxesPrecJul.png'), getTestDirPath('BoxesPrecJan.png')
+
+def getBoxesInputsSwappedPrecipitations():
+    return getTestDirPath('BoxesTempJul.png'), getTestDirPath('BoxesTempJan.png'), getTestDirPath('BoxesPrecJan.png'), getTestDirPath('BoxesPrecJul.png')
+
 def test1Fn():
     tempProf = InputProfile(tColorTableDefault, defaultOceanColor)
     precProf = InputProfile(pColorTableDefault, defaultOceanColor)
@@ -39,9 +45,69 @@ def test1Clean():
     dPaths = [outputPath]
     deleteFiles(dPaths)
 
+def test2Fn():
+    tempProf = InputProfile(tColorTableDefault, defaultOceanColor)
+    precProf = InputProfile(pColorTableDefault, defaultOceanColor)
+    outProf = readOutputProfile(getParentDirPath('altOutputProfile'))
+
+    outputPath = getTestDirPath('test2-out.png')
+    tempnsPath, tempnwPath, precnsPath, precnwPath = getProfiaInputs()
+
+    comparePath = getTestDirPath('ProfiaOutputAlt.png')
+
+    outputToFile(outputPath, buildClimates(tempnsPath, tempnwPath, precnsPath, precnwPath, tempProf, precProf, outProf))
+
+    return compareImages(outputPath, comparePath)
+
+def test2Clean():
+    outputPath = getTestDirPath('test2-out.png')
+    dPaths = [outputPath]
+    deleteFiles(dPaths)
+
+def test3Fn():
+    tempProf = InputProfile(tColorTableDefault, defaultOceanColor)
+    precProf = InputProfile(pColorTableDefault, defaultOceanColor)
+    outProf = readOutputProfile(getParentDirPath('defaultOutputProfile'))
+
+    outputPath = getTestDirPath('test3-out.png')
+    tempnsPath, tempnwPath, precnsPath, precnwPath = getBoxesInputs()
+
+    comparePath = getTestDirPath('BoxesOutputDefault.png')
+
+    outputToFile(outputPath, buildClimates(tempnsPath, tempnwPath, precnsPath, precnwPath, tempProf, precProf, outProf))
+                                
+    return compareImages(outputPath, comparePath)
+
+def test3Clean():
+    outputPath = getTestDirPath('test3-out.png')
+    dPaths = [outputPath]
+    deleteFiles(dPaths)
+
+def test4Fn():
+    tempProf = InputProfile(tColorTableDefault, defaultOceanColor)
+    precProf = InputProfile(pColorTableDefault, defaultOceanColor)
+    outProf = readOutputProfile(getParentDirPath('defaultOutputProfile'))
+
+    outputPath = getTestDirPath('test4-out.png')
+    tempnsPath, tempnwPath, precnsPath, precnwPath = getBoxesInputsSwappedPrecipitations()
+
+    comparePath = getTestDirPath('BoxesOutputSwapped.png')
+
+    outputToFile(outputPath, buildClimates(tempnsPath, tempnwPath, precnsPath, precnwPath, tempProf, precProf, outProf))
+
+    return compareImages(outputPath, comparePath)
+
+def test4Clean():
+    outputPath = getTestDirPath('test4-out.png')
+    dPaths = [outputPath]
+    deleteFiles(dPaths)
+
 def runAll(doCleanup):
     tests = []
     tests.append(ImgTest('All default profiles - Profia test', test1Fn, test1Clean))
+    tests.append(ImgTest('Alternate output profile - Profia test', test2Fn, test2Clean))
+    tests.append(ImgTest('All default profiles - Boxes test', test3Fn, test3Clean))
+    tests.append(ImgTest('All default profiles - Boxes test (swapped summer/winter precipitation)', test4Fn, test4Clean))
     runTests(tests, doCleanup)
 
 if __name__ == '__main__':
